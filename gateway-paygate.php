d90024c075e9f9deebeafa926010cbec4c5d6164
Bruce Atkinson <bruce@appinlet.com>
2018-12-01 00:43:13 +0200

    Seperate iFrame branch

--- gateway-paygate.php
+++ gateway-paygate.php
@@ -1,17 +1,16 @@
 <?php
-
 /**
- * Plugin Name: PayGate PayWeb3 plugin for WooCommerce (Redirect)
- * Plugin URI: https://github.com/PayGate/PayWeb_WooCommerce
+ * Plugin Name: PayGate PayWeb3 plugin for WooCommerce (iFrame)
+ * Plugin URI: https://www.paygate.co.za
  * Description: Accept payments for WooCommerce using PayGate's PayWeb3 service
- * Version: 1.1.7
+ * Version: 1.1.6
  * Author: PayGate (Pty) Ltd
  * Author URI: https://www.paygate.co.za/
  * Developer: App Inlet (Pty) Ltd
  * Developer URI: https://www.appinlet.com/
  *
  * WC requires at least: 2.6
- * WC tested up to: 3.5
+ * WC tested up to: 3.3
  *
  * Copyright: © 2018 PayGate (Pty) Ltd.
  * License: GNU General Public License v3.0
@@ -26,6 +25,10 @@ add_action( 'plugins_loaded', 'woocommerce_paygate_init', 0 );
  * @since 1.0.0
  */
 
+if ( !defined( 'PAYGATE_PLUGIN_URL' ) ) {
+    define( 'PAYGATE_PLUGIN_URL', plugin_dir_url( __FILE__ ) );
+}
+
 function woocommerce_paygate_init()
 {
 
@@ -36,29 +39,6 @@ function woocommerce_paygate_init()
     require_once plugin_basename( 'classes/paygate.class.php' );
 
     add_filter( 'woocommerce_payment_gateways', 'woocommerce_add_paygate_gateway' );
-
-    require_once 'classes/updater.class.php';
-
-    if ( is_admin() ) { // note the use of is_admin() to double check that this is happening in the admin
-
-        $config = array(
-            'slug' => plugin_basename( __FILE__ ),
-            'proper_folder_name' => 'woocommerce-gateway-paygate-pw3',
-            'api_url' => 'https://api.github.com/repos/PayGate/PayWeb_WooCommerce',
-            'raw_url' => 'https://raw.github.com/PayGate/PayWeb_WooCommerce/master',
-            'github_url' => 'https://github.com/PayGate/PayWeb_WooCommerce',
-            'zip_url' => 'https://github.com/PayGate/PayWeb_WooCommerce/archive/master.zip',
-            'homepage' => 'https://github.com/PayGate/PayWeb_WooCommerce',
-            'sslverify' => true,
-            'requires' => '4.0',
-            'tested' => '4.9.8',
-            'readme' => 'README.md',
-            'access_token' => '',
-        );
-
-        new WP_GitHub_Updater( $config );
-
-    }
 
 } // End woocommerce_paygate_init()
 
@@ -71,7 +51,7 @@ function woocommerce_paygate_init()
 function woocommerce_add_paygate_gateway( $methods )
 {
 
-    $methods[] = 'WC_Gateway_PayGate';
+    $methods[] = WC_Gateway_PayGate::instance();
 
     return $methods;
 
