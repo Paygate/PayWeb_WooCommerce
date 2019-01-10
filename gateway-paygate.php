9643d4daec16248801bdf2eb665c242b4f710232
Bruce Atkinson <bruce@appinlet.com>
2019-01-10 22:31:33 +0200

    Code format

--- gateway-paygate.php
+++ gateway-paygate.php
@@ -40,21 +40,22 @@ function woocommerce_paygate_init()
 
     require_once 'classes/updater.class.php';
 
-    if ( is_admin() ) { // note the use of is_admin() to double check that this is happening in the admin
+    if ( is_admin() ) {
+        // note the use of is_admin() to double check that this is happening in the admin
 
         $config = array(
-            'slug' => plugin_basename( __FILE__ ),
+            'slug'               => plugin_basename( __FILE__ ),
             'proper_folder_name' => 'woocommerce-gateway-paygate-pw3',
-            'api_url' => 'https://api.github.com/repos/PayGate/PayWeb_WooCommerce',
-            'raw_url' => 'https://raw.github.com/PayGate/PayWeb_WooCommerce/master',
-            'github_url' => 'https://github.com/PayGate/PayWeb_WooCommerce',
-            'zip_url' => 'https://github.com/PayGate/PayWeb_WooCommerce/archive/master.zip',
-            'homepage' => 'https://github.com/PayGate/PayWeb_WooCommerce',
-            'sslverify' => true,
-            'requires' => '4.0',
-            'tested' => '5.0.3',
-            'readme' => 'README.md',
-            'access_token' => '',
+            'api_url'            => 'https://api.github.com/repos/PayGate/PayWeb_WooCommerce',
+            'raw_url'            => 'https://raw.github.com/PayGate/PayWeb_WooCommerce/master',
+            'github_url'         => 'https://github.com/PayGate/PayWeb_WooCommerce',
+            'zip_url'            => 'https://github.com/PayGate/PayWeb_WooCommerce/archive/master.zip',
+            'homepage'           => 'https://github.com/PayGate/PayWeb_WooCommerce',
+            'sslverify'          => true,
+            'requires'           => '4.0',
+            'tested'             => '5.0.3',
+            'readme'             => 'README.md',
+            'access_token'       => '',
         );
 
         new WP_GitHub_Updater( $config );
