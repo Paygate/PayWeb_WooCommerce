5f6a4940efdd73d1cb2149314aee00c347b9b5eb
Bruce Atkinson <bruce@appinlet.com>
2021-05-13 23:01:53 +0200

    = 1.4.3 - May 13, 2021
    * Tested on WooCommerce 5.3 and Wordpress 5.7.
    * Add SnapScan payment type.
    * Remove legacy reference to 'paypopup'.
    * Fix error messaging on cancelled and declined transactions.
    * Use pending status for checksum failures.
    * Add cron job for query function on orders older than 60 minutes.
    * Fixed incorrect order note on transaction declined for Notify method.

--- classes/WC_Gateway_PayGate_Cron.php
+++ classes/WC_Gateway_PayGate_Cron.php
@@ -9,6 +9,8 @@
 
 class WC_Gateway_PayGate_Cron extends WC_Gateway_PayGate
 {
+    const LOGGING = 'logging';
+
     /**
      *
      */
@@ -16,25 +18,51 @@ class WC_Gateway_PayGate_Cron extends WC_Gateway_PayGate
     {
         $gwp = new WC_Gateway_PayGate_Portal();
 
-        $orders = self::paygate_order_query_cron_query();
-        foreach ($orders as $order_id) {
-            $order = new WC_Order($order_id->ID);
-            $notes = $gwp->getOrderNotes($order_id->ID);
+        // Load the settings
+        $settings = get_option('woocommerce_paygate_settings', false);
 
-            $payRequestId = self::getPayRequestIdNotes($notes);
+        $logging = false;
+
+        if (isset($settings[self::LOGGING]) && $settings[self::LOGGING] === 'yes') {
+            $logging = true;
+        }
+
+        $logger = wc_get_logger();
+        $logging ? $logger->add('payweb_cron', 'Starting cron job') : '';
+
+        $orders = self::paygate_order_query_cron_query();
+        $logging ? $logger->add('payweb_cron', 'Orders: ' . serialize($orders)) : '';
+
+        foreach ($orders as $order_id) {
+            $order   = null;
+            $orderId = $order_id->ID;
+            $logging ? $logger->add('payweb_cron', 'Order ID: ' . $orderId) : '';
+            try {
+                $order = new WC_Order($orderId);
+                $logging ? $logger->add('payweb_cron', 'Order: ' . serialize($order)) : '';
+            } catch (Exception $e) {
+                $logging ? $logger->add('payweb_cron', 'Fatal error: ' . $e->getMessage()) : '';
+            }
+
+            $notes = self::getOrderNotes($orderId);
+
+            $payRequestId = WC_Gateway_PayGate_Portal::getPayRequestIdNotes($notes);
+            $logging ? $logger->add('payweb_cron', 'PayRequestId: ' . $payRequestId) : '';
 
             if ($payRequestId == '') {
-                return;
+                break;
             }
 
             $response          = $gwp->paywebQuery($payRequestId, $order);
             $transactionStatus = $gwp->paywebQueryStatus($response);
             $responseText      = $gwp->paywebQueryText($response, $payRequestId);
 
+            $logging ? $logger->add('payweb_cron', 'Response Text: ' . $responseText) : '';
+
             if ((int)$transactionStatus === 1) {
                 if ( ! $order->has_status(self::PROCESSING) && ! $order->has_status(self::COMPLETED)) {
                     $order->payment_complete();
-                    $responseText .= "<br>Order set to \"Processing\"";
+                    $responseText .= "<br>Order set to \"Processing\" by PayWeb Cron";
                 }
             } else {
                 if ( ! $order->has_status(self::FAILED)) {
@@ -49,12 +77,16 @@ class WC_Gateway_PayGate_Cron extends WC_Gateway_PayGate
     {
         global $wpdb;
 
+        // Orders from the last 60 minutes
+        $ordersModifiedInLastHour = " " . date('Y-m-d H:i:s', strtotime('-60 minutes')) . "";
+
         $query = <<<QUERY
 SELECT ID FROM `{$wpdb->prefix}posts`
 INNER JOIN `{$wpdb->prefix}postmeta` ON {$wpdb->prefix}posts.ID = {$wpdb->prefix}postmeta.post_id
 WHERE meta_key = '_payment_method'
 AND meta_value = 'paygate'
 AND post_status = 'wc-pending'
+AND post_date_gmt < '$ordersModifiedInLastHour'
 QUERY;
 
         return $wpdb->get_results($query);
